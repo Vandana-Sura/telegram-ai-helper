9a495917611428c593db899369cb9e6e9904be82
Vandana-Sura <vandanasura6@gmail.com>
2025-02-02 12:11:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+
+# API Configuration
+API_ID = 24133783
+API_HASH = "b34262b7e2d03332ae99cb3aebfc66de"
+BOT_TOKEN = "7734285212:AAF-JH44YALHy83jxWQa9LESkgoCkfn3TcU"
+GOOGLE_API_KEY = "AIzaSyAZNlFuGqbepbceQx9_jHBOxJQxU_effQU"
+MONGO_URI = "mongodb+srv://Telegram_aibot:<Vandana@123>@cluster0.daga7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "telegram_chatbot_db"
+GOOGLE_CLOUD_ID="AIzaSyB6F63Diduzn7Nm_HVR-uO-X8Qj11yN50I"
+GOOGLE_CSE_ID = "2726932ea241547e1"  # Your Google Custom Search Engine ID
+PUBLIC_URL = "https://cse.google.com/cse?cx=2726932ea241547e1"  # Public URL for the search or any other URL if required
+
